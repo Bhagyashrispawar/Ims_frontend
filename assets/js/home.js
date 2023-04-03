c946492d19ef7033f93b5de2214cb760a927609a
bhartipatil15 <bhartiipatil15@gmail.com>
2023-04-03 23:56:47 +0530

    added 4 folders and main files

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,4 +5,7 @@ $(document).ready(function() {
     $("#left-sidebar").load("./../../templates/left-sidebar/left-sidebar.html");
     $("#layoutSidenav_content").load("./../../templates/dashboard/dashboard.html");
     $("#ITTRequest_content").load("./../../templates/ITTRequest/ITTRequest.html");
+    $("#PhysicalGaugingReport_content").load("./../../templates/PhysicalGaugingReport/PhysicalGaugingReport.html");
+    $("#RakeOTRSummary_content").load("./../../templates/RakeOTRSummary/RakeUnloadingSummary.html");
+    $("#ITTSummary_content").load("./../../templates/ITTSummary/ITTSummary.html");
 });
