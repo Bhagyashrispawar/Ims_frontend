c0a0f15f28e18db9ac945ae15a5e57f57ca38f20
Bhagyashri0411 <bhagyashripawar0411@gmail.com>
2023-04-05 19:54:49 +0530

    dowload pdf when we click on report

--- templates/Header/header.js
+++ templates/Header/header.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
-
-
+    onListheader()
+    getDateTime()
+    
+});
+function onListheader (){
 
     $.ajax({
         url: "./../../assets/js/list.json",
@@ -9,7 +12,7 @@ $(document).ready(function () {
                 var name = item.name; //this should be in div class = "card-header"
                 var headerIndex = item.numberIndex
                 var items = item.items; //this should be in div class = "card-header"
-
+    
                 var html = '<div class="dropdown">';
                 html += `<button class="btn  dropdown-toggle dropdown-btn" type="button" id=${headerIndex} data-bs-toggle="dropdown" aria-expanded="false">` + name + `</button>`;
                 html += `<ul class="dropdown-menu" aria-labelledby=${headerIndex}>`;
@@ -18,14 +21,45 @@ $(document).ready(function () {
                 })
                 html += '</ul>';
                 html += '</div>';
-
+    
                 $("#id").append(html);
-
+    
             });
-
+    
         }
     });
+}
 
 
+function getDateTime() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var day = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    if (month.toString().length == 1) {
+        month = '0' + month;
+    }
+    if (day.toString().length == 1) {
+        day = '0' + day;
+    }
+    if (hour.toString().length == 1) {
+        hour = '0' + hour;
+    }
+    if (minute.toString().length == 1) {
+        minute = '0' + minute;
+    }
+    if (second.toString().length == 1) {
+        second = '0' + second;
+    }
+    var dateTime = day + '/' + month + '/' + year + ' ' + hour + ':' + minute + ':' + second;
+    return dateTime;
+}
 
-});
+// example usage: realtime clock
+setInterval(function () {
+    currentTime = getDateTime();
+    document.getElementById("datetime").innerHTML = currentTime;
+}, 1000);
