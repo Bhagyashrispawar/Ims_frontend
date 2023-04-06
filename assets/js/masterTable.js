77110d42d131cefc95e43084a45568836fe9aa0d
bhartipatil15 <bhartiipatil15@gmail.com>
2023-04-06 17:37:31 +0530

    added files again

--- assets/js/masterTable.js
+++ assets/js/masterTable.js
@@ -2,6 +2,13 @@ $(document).ready(function () {
     $("#nav").load("./../../templates/Header/header.html", function () { });
 
     $("#pipelineTable").load("./../../templates/Mastertablesub/pipelinevolumn.html");
+    $("#productTable").load("./../../templates/Mastertablesub/ProductMaster.html");
+    $("#pumpTable").load("./../../templates/Mastertablesub/PumpMaster.html");
+    $("#supplierTable").load("./../../templates/Mastertablesub/SupplierMaster.html");
+    $("#surveyTable").load("./../../templates/Mastertablesub/SurveyMaster.html");
+    $("#tankCalibrationTable").load("./../../templates/Mastertablesub/TankCalibrationMaster.html");
+    $("#tankTable").load("./../../templates/Mastertablesub/TankMaster.html");
+    $("#userTable").load("./../../templates/Mastertablesub/UserMaster.html");
 
 
 });
